2eaed01b051fc70ca51529926863cd6fd5f156de
Soman Anshuman <soman.a@idsil.loc>
2025-04-08 10:24:16 +0530

    learning day 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from "./signup/signup.component";
+import { ProfileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, LoginComponent, SignupComponent, ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-first-angular-app';
+  name = '';
+  x = 10;
+  y = 20;
+  user1 = 'sOmAn';
 }
