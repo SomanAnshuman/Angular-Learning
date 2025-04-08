37fa24c97bdbe37fec51ecaae9693ab41589c55f
Soman Anshuman <soman.a@idsil.loc>
2025-04-08 11:53:49 +0530

    day 2 function calls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from "./signup/signup.component";
-import { ProfileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule, LoginComponent, SignupComponent, ProfileComponent],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  name = '';
-  x = 10;
-  y = 20;
-  user1 = 'sOmAn';
+  handleClickEvent() {
+    console.log('Function called by click');
+    this.otherFunction();
+  }
+
+  otherFunction() {
+    console.log('other function');
+  }
+
+  sum(a: number, b: number) {
+    console.log(a + b);
+  }
 }
