0a09c9a33935c4f2d8326bd3b8524a8670d0eaa1
Soman Anshuman <soman.a@idsil.loc>
2025-04-14 18:06:17 +0530

    day 3 ngFor Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule],
+  imports: [NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  name = 's'
+  students = ['Anil', 'Sam', 'Peter', 'Vinay', 'Bruce'];
+
+  studentsData = [
+    { name: 'Anil', age: 29, email: 'anil@test.com' },
+    { name: 'Sam', age: 43, email: 'sam@test.com' },
+    { name: 'Peter', age: 12, email: 'peter@test.com' },
+    { name: 'Bruce', age: 89, email: 'bruce@test.com' },
+    { name: 'Tony', age: 65, email: 'tony@test.com' },
+  ];
 }
