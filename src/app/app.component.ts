b30d9567f9b9cf987b30f8ac1712827392f06ef7
Soman Anshuman <soman.a@idsil.loc>
2025-04-14 18:33:44 +0530

    day 3 ngIf Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,17 @@
 import { Component } from '@angular/core';
-import { NgFor } from '@angular/common';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [NgFor],
+  imports: [NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  students = ['Anil', 'Sam', 'Peter', 'Vinay', 'Bruce'];
+  login = true;
 
-  studentsData = [
-    { name: 'Anil', age: 29, email: 'anil@test.com' },
-    { name: 'Sam', age: 43, email: 'sam@test.com' },
-    { name: 'Peter', age: 12, email: 'peter@test.com' },
-    { name: 'Bruce', age: 89, email: 'bruce@test.com' },
-    { name: 'Tony', age: 65, email: 'tony@test.com' },
-  ];
+  block = 0;
+  updateBlock(){
+    this.block++;
+  }
 }
