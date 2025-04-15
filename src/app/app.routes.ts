f022fa537d093fbac4b59cb9c04c5e5d84392785
Soman Anshuman <soman.a@idsil.loc>
2025-04-15 12:44:18 +0530

    day 4 Routing with a header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,10 @@ import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
 import { ContactComponent } from './contact/contact.component';
 import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+    {path: '', component: HomeComponent},
     {path: 'about', component: AboutComponent},
     {path: 'login', component: LoginComponent},
     {path: 'contact', component: ContactComponent},
