57af646238cf13c0da58c1b35fea48b492310dba
Soman Anshuman <soman.a@idsil.loc>
2025-04-15 12:56:42 +0530

    day 4 handling page not found | 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './login/login.component';
 import { ContactComponent } from './contact/contact.component';
 import { SignupComponent } from './signup/signup.component';
 import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
@@ -11,4 +12,5 @@ export const routes: Routes = [
     {path: 'login', component: LoginComponent},
     {path: 'contact', component: ContactComponent},
     {path: 'signup', component: SignupComponent},
+    {path: '**', component: PageNotFoundComponent}, //wild card routing
 ];
