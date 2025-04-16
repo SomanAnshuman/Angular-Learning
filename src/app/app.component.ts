7904d8a6784f0c94cc64579f5e0f024621bf7af5
Soman Anshuman <soman.a@idsil.loc>
2025-04-16 18:50:50 +0530

    day 5 Template driven forms Validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
