73b1eb94ea0090a2262a0c879f7a79e985c5777a
Soman Anshuman <soman.a@idsil.loc>
2025-04-16 12:31:21 +0530

    day 5 Basics of Reactive forms (formControl)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+  name = new FormControl('soman');
+  password = new FormControl('123');
 
+  displayValue() {
+    console.log(this.name.value, this.password.value);
+  }
+
+  setValues() {
+    this.name.setValue('Peter');
+    this.password.setValue('3000');
+  }
 }
