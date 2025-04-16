71d017a6eb9aa54a58197fff9378b317e5e11eb7
Soman Anshuman <soman.a@idsil.loc>
2025-04-16 15:59:01 +0530

    day 5 Template driven forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,17 @@
-import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import {
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [ReactiveFormsModule, NgIf],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  profileForm = new FormGroup({
-    name: new FormControl('', [Validators.required]),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.minLength(5),
-    ]),
-    email: new FormControl('', [
-      Validators.required,
-      Validators.maxLength(50),
-      Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$'),
-    ]),
-  });
-
-  onSubmit() {
-    console.log('Submit func called');
-    console.log(this.profileForm.value);
-  }
-
-  get name() {
-    return this.profileForm.get('name');
-  }
-  get password() {
-    return this.profileForm.get('password');
-  }
-  get email() {
-    return this.profileForm.get('email');
+  userDetails: any = {};
+  
+  addDetails(val: NgForm){
+    console.log(val);
+    this.userDetails = val;
   }
 }
